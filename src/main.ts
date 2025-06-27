0fcefb6ed15b279b32541d6212d7014d3ea0d050
Supat Baingam <61405117+Spdevelop@users.noreply.github.com>
2025-06-27 20:31:11 +0700

    [Fixed] enableCors all

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { SpdevLogger } from './core/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
   new SpdevLogger().log(
