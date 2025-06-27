4bcf65b4e7325cf7b7e9365db8cec2cbddd48fbe
Supat Baingam <61405117+Spdevelop@users.noreply.github.com>
2025-06-27 23:56:51 +0700

    [TEST] allowedHeaders

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 import { SpdevLogger } from './core/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
   });
 
   await app.listen(process.env.PORT ?? 3000);
